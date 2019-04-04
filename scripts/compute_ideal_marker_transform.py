defc7c726b9948944b176556caca9d78700ed73b
John Wason <wason@wasontech.com>
2019-04-04 13:43:57 -0400

    first commit

---
+++ scripts/compute_ideal_marker_transform.py
@@ -0,0 +1,56 @@
+import rospy
+import cv2
+import sys
+import general_robotics_toolbox as rox
+from sensor_msgs.msg import CameraInfo
+import numpy as np
+from general_robotics_toolbox import ros_msg as rox_msg
+from pbvs_object_placement import get_all_payload_markers, get_aruco_dictionary, \
+    get_aruco_gridboard, detect_marker, get_camera_info
+
+def main():
+
+    rospy.init_node("compute_ideal_marker_transform", anonymous=True)
+    
+    fname = sys.argv[1]
+    img = cv2.imread(fname)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        
+    fixed_marker_name = sys.argv[2]
+    payload_marker_name = sys.argv[3]
+    camera_info_topic = sys.argv[4]
+        
+    print fixed_marker_name
+    print payload_marker_name
+    
+    markers = get_all_payload_markers()
+    
+    fixed_marker = markers[fixed_marker_name]
+    payload_marker = markers[payload_marker_name]
+    
+    print fixed_marker
+    print payload_marker
+    
+    aruco_dict = get_aruco_dictionary(fixed_marker)
+    camera_info = get_camera_info(camera_info_topic)
+    
+    fixed_marker_transform = detect_marker(img, fixed_marker,camera_info, aruco_dict)
+    payload_marker_transform = detect_marker(img, payload_marker, camera_info, aruco_dict)
+    
+    print fixed_marker_transform
+    print payload_marker_transform
+    
+    tag_to_tag_transform =  fixed_marker_transform.inv()*payload_marker_transform
+    tag_to_tag_transform.parent_frame_id = fixed_marker_name
+    tag_to_tag_transform.child_frame_id = payload_marker_name
+    print tag_to_tag_transform
+    
+    ros_tf = rox_msg.transform2transform_stamped_msg(tag_to_tag_transform)
+    print ros_tf
+    
+
+if __name__ == "__main__":
+    main()
+
+
+
